46619c9c52f9fcb17940e691cf49d7de35db4075
Austin Calvelage <austin.calvelage@icloud.com>
2023-02-05 09:12:52 -0500

    Added framerMotion for animation, replaced gradient of BG with shadows

--- pages/components/SwitchButton.tsx
+++ pages/components/SwitchButton.tsx
@@ -1,18 +1,10 @@
 import { useState } from 'react'
 import cn from 'classnames'
 import useSound from 'use-sound'
+import { motion } from 'framer-motion'
 
-type Props = {
-  isOn?: boolean
-}
-
-const SwitchButton = ({ isOn }: Props): JSX.Element => {
-  const [switchState, setSwitchState] = useState(isOn)
-
-  function sliderHandler(event: React.MouseEvent<HTMLElement>) {
-    setSwitchState((prevState) => !prevState)
-    switchState ? on() : off()
-  }
+const SwitchButton = () => {
+  const [isOn, setIsOn] = useState(false)
 
   const [on] = useSound('./sounds/click1.mp3', {})
 
@@ -23,23 +15,26 @@ const SwitchButton = ({ isOn }: Props): JSX.Element => {
   return (
     <div className='rounded-full bg-gradient-to-b from-[#EFEFEF] to-[#FFFFFF] p-1 shadow-[inset_0_1px_1px_rgb(0_0_0_/_0.02),_0_8px_16px_rgb(0_0_0_/_0.02)] select-none'>
       <button
-        onClick={sliderHandler}
-        className={cn(
-          'relative flex h-[20px] w-[36px] items-center justify-center rounded-full p-px',
-          // 'before:h-full before:w-1/2 before:bg-gradient-to-br before:from-[#28bf98] before:via-[#25e8b9] before:to-[#FFFFFF] before:rounded-tl-full before:rounded-bl-full',
-          // 'after:h-full after:w-1/2 after:bg-gradient-to-bl after:from-[#999999] after:via-[#afafaf] after:to-[#FFFFFF] after:rounded-tr-full after:rounded-br-full'
-          {
-            'bg-gradient-to-b from-[#28bf98] via-[#25e8b9] to-[#FFFFFF]': switchState,
-            'bg-gradient-to-b from-[#999999] via-[#afafaf] to-[#FFFFFF]': !switchState,
-          }
-        )}
+        onClick={() => setIsOn(!isOn)}
+        className={cn('relative flex h-[20px] w-[36px] items-center rounded-full transition-shadow', {
+          'bg-gradient-to-b from-[#8EEECF] via-[#25e8b9] to-[#FFFFFF] justify-end shadow-[inset_0px_0px_8px_rgb(87_181_148_/_0.8),_inset_0px_1px_1px_0.5px_rgb(87_181_148_/_0.3),_0px_0px_4px_rgb(110_229_187_/_0.75)]':
+            isOn,
+          'bg-gradient-to-b from-[#AAAAAA] via-[#afafaf] to-[#FFFFFF] shadow-[inset_0px_0px_8px_rgb(0_0_0_/_0.2),_inset_0px_1px_1px_0.5px_rgb(0_0_0_/_0.1)] justify-start':
+            !isOn,
+        })}
       >
-        <span
+        <motion.span
+          layout
+          transition={{ type: 'spring', stiffness: 700, damping: 30 }}
           className={cn(
-            'absolute z-[3] flex h-[21px] w-[21px] items-center justify-center rounded-full bg-gradient-to-b from-[#E0E0E0] to-[#FFFFFF] p-px shadow-lg transition-transform ease-linear',
-            { 'translate-x-[-8px]': switchState },
-            { 'translate-x-[8px]': !switchState },
-            'before:relative before:h-full before:w-full before:rounded-full before:bg-[#EAEAEA]'
+            'block -m-px absolute h-[21px] w-[21px] rounded-full bg-gradient-to-b from-[#FFFFFF] to-[#ECECEC] transition-shadow',
+            'before:rounded-full before:absolute before:inset-0.5 before:bg-gradient-to-b before:from-[#E1E1E1] before:to-[#FBFBFB]',
+            {
+              'shadow-[inset_0px_-1px_3px_rgb(0_0_0_/_0.25),_0px_0px_0px_0.5px_rgb(0_0_0_/_0.075),_0px_2px_4px_rgb(0_0_0_/_0.1)]':
+                !isOn,
+              'shadow-[inset_0px_-1px_3px_rgb(0_0_0_/_0.25),_0px_0px_0px_0.5px_rgb(0_0_0_/_0.075),_0px_2px_4px_rgb(0_0_0_/_0.1),_inset_1px_0px_2px_-1px_rgb(110_229_187/_0.98)]':
+                isOn,
+            }
           )}
         />
       </button>
@@ -48,3 +43,5 @@ const SwitchButton = ({ isOn }: Props): JSX.Element => {
 }
 
 export default SwitchButton
+
+// {momentary - pops back up after press and maintained - press it and stays in}
